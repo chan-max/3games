8352ea5ae1fb9c319f352f2836e4c0a3dff448db
chan-max <jackieontheway666@gmail.com>
2024-04-17 17:48:39 +0800

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import './style/index.less'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+import { Application } from 'pixi.js';
+
+// Asynchronous IIFE
+(async () =>
+{
+    // Create a PixiJS application.
+    const app = new Application();
+
+    // Intialize the application.
+    await app.init({ background: '#1099bb', resizeTo: window });
+
+    // Then adding the application's canvas to the DOM body.
+    document.body.appendChild(app.canvas);
+})();
